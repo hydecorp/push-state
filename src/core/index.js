a8c9a4b552c7ad368379a5df136608b7776a5203
Florian Klampfer <f.klampfer@gmail.com>
2016-10-10 17:32:37 +0200

    Try different approach

--- src/core/index.js
+++ src/core/index.js
@@ -11,33 +11,48 @@ import componentCore from 'y-component/src/component-core';
 
 import { Observable } from 'rxjs-es/Observable';
 import { Subject } from 'rxjs-es/Subject';
+// import { asap } from 'rxjs-es/scheduler/asap';
 
+import 'rxjs-es/add/observable/defer';
 import 'rxjs-es/add/observable/empty';
 import 'rxjs-es/add/observable/fromEvent';
 import 'rxjs-es/add/observable/merge';
+// import 'rxjs-es/add/observable/never';
 import 'rxjs-es/add/observable/of';
 import 'rxjs-es/add/observable/throw';
 
 import 'rxjs-es/add/observable/dom/ajax';
 
-import 'rxjs-es/add/operator/auditTime';
+// import 'rxjs-es/add/operator/auditTime';
 import 'rxjs-es/add/operator/catch';
+// import 'rxjs-es/add/operator/concat';
+// import 'rxjs-es/add/operator/combineLatest';
 import 'rxjs-es/add/operator/delay';
-import 'rxjs-es/add/operator/distinctUntilChanged';
+// import 'rxjs-es/add/operator/distinctUntilChanged';
+import 'rxjs-es/add/operator/distinctUntilKeyChanged';
 import 'rxjs-es/add/operator/do';
 import 'rxjs-es/add/operator/filter';
 import 'rxjs-es/add/operator/map';
+// import 'rxjs-es/add/operator/mapTo';
+import 'rxjs-es/add/operator/merge';
 import 'rxjs-es/add/operator/mergeAll';
-import 'rxjs-es/add/operator/pairwise';
-import 'rxjs-es/add/operator/publishLast';
-import 'rxjs-es/add/operator/retry';
+// import 'rxjs-es/add/operator/pairwise';
+// import 'rxjs-es/add/operator/publish';
+// import 'rxjs-es/add/operator/publishLast';
+// import 'rxjs-es/add/operator/repeat';
+// import 'rxjs-es/add/operator/retry';
+import 'rxjs-es/add/operator/share';
+// import 'rxjs-es/add/operator/skipUntil';
 import 'rxjs-es/add/operator/startWith';
+// import 'rxjs-es/add/operator/subscribeOn';
 import 'rxjs-es/add/operator/switch';
 import 'rxjs-es/add/operator/switchMap';
+import 'rxjs-es/add/operator/take';
+// import 'rxjs-es/add/operator/takeUntil';
 import 'rxjs-es/add/operator/withLatestFrom';
 import 'rxjs-es/add/operator/zip';
 
-import { shouldLoadAnchor, getScrollTop, getScrollHeight, querySelectorInv } from '../common';
+import { shouldLoadAnchor, getScrollTop, getScrollHeight } from '../common';
 
 // requirements
 // object.assign, queryslector
@@ -49,6 +64,30 @@ window.Observable = Observable;
 function minDur(time) {
   return this.zip(Observable.of(null).delay(time))
     .map(([$]) => $);
+}
+
+class Kind {
+  constructor(event) {
+    this.event = event;
+  }
+}
+class Push extends Kind {
+  constructor(event) {
+    super(event);
+    this.href = event.currentTarget.href;
+  }
+}
+class Hint extends Kind {
+  constructor(event) {
+    super(event);
+    this.href = event.currentTarget.href;
+  }
+}
+class Pop extends Kind {
+  constructor(event) {
+    super(event);
+    this.href = window.location.href;
+  }
 }
 
 // ~ mixin smoothStateCore with componentCore { ...
@@ -82,13 +121,7 @@ export default C => class extends componentCore(C) {
   }
 
   bindCallbacks() {
-    def(this, 'ajaxResponseToContent', { value: this.ajaxResponseToContent.bind(this) });
-    def(this, 'beNice', { value: this.beNice.bind(this) });
-    def(this, 'beOkay', { value: this.beOkay.bind(this) });
-    def(this, 'hrefToRequestData', { value: this.hrefToRequestData.bind(this) });
-    def(this, 'makeRequest', { value: this.makeRequest.bind(this) });
-    def(this, 'makePrefetchRequest', { value: this.makePrefetchRequest.bind(this) });
-    def(this, 'usePrefechOrMakeRequest', { value: this.usePrefechOrMakeRequest.bind(this) });
+    def(this, 'fetchPage', { value: this.fetchPage.bind(this) });
     def(this, 'onBefore', { value: this.onBefore.bind(this) });
     def(this, 'onAfter', { value: this.onAfter.bind(this) });
     def(this, 'onError', { value: this.onError.bind(this) });
@@ -124,60 +157,156 @@ export default C => class extends componentCore(C) {
     def(this, 'titleElement', { value: document.querySelector('title') || {} });
   }
 
-  layPipes() {
-    const pushstate$ = Observable.fromEvent(this.el, 'click')
-      .map(event => ({
-        event,
-        anchor: querySelectorInv(event.target, this.linkSelector),
-      }))
-      .filter(this.beNice)
-      .do(() => this.saveScrollPosition())
-      .do(({ event }) => event.preventDefault())
-      .map(({ anchor }) => ({
-        push: true,
-        href: anchor.href,
-      }));
-
-    const popstate$ = Observable.fromEvent(window, 'popstate')
-      .filter(() => window.history.state != null)
-      .do(event => event.preventDefault())
-      .map(() => ({
-        push: false,
-        href: window.location.href,
-      }));
-
-    const prefetchHref$$ = new Subject();
-    const prefetch$$ = prefetchHref$$
-      .switch()
-      // .filter(() => !this.isLoading) // TODO: this works, but isn't so great...
-      .distinctUntilChanged() // don't cancel request if user jiggels over link
-      .map(href => ({ href }))
-      .map(this.hrefToRequestData)
-      .map(this.makePrefetchRequest)
-      .pairwise()
-      .map(([prev, curr]) => {
-        if (prev.conn) prev.conn.unsubscribe();
-        return curr;
-      })
-      .startWith({});
-
-    Observable.merge(pushstate$, popstate$)
-      .do(this.onBefore)
-      .map(this.hrefToRequestData)
-      .withLatestFrom(prefetch$$)
-      .map(this.usePrefechOrMakeRequest)
-      .map(ajax => ajax.catch(this.onError))
-      .switch()
-      .map(this.ajaxResponseToContent)
-      .do(this.updateDOM)
-      .do(this.onAfter)
-      .subscribe(() => {
-        const link$ = this.linkObservable();
-        prefetchHref$$.next(this.bindPrefetchEvents(link$));
+  bindPushEvents(link$) {
+    return Observable.defer(() => this.fromEvent(link$, 'click'))
+      .mergeAll()
+      .map(event => new Push(event))
+      .filter(({ event }) => this.beNice(event))
+      .do(({ event }) => {
+        this.saveScrollPosition();
+        event.preventDefault();
       });
+  }
 
+  bindHintEvents(link$) {
+    return Observable.defer(() => Observable.empty()
+      .merge(this.fromEvent(link$, 'mouseenter'))
+      .merge(this.fromEvent(link$, 'touchstart'))
+      .merge(this.fromEvent(link$, 'focus'))
+    )
+      .mergeAll()
+      .map(event => new Hint(event))
+      .filter(({ event }) => this.beOkay(event));
+  }
+
+  bindPopstateEvent() {
+    return Observable.fromEvent(window, 'popstate')
+      .map(event => new Pop(event))
+      .filter(() => window.history.state != null);
+  }
+
+  linkObservable() {
+    return Observable.of(this.el.querySelectorAll(this.linkSelector));
+  }
+
+  fromEvent(link$, event) {
+    return link$.map(link => Observable.fromEvent(link, event));
+  }
+
+  fetchPage(kind) {
+    const requestData = this.hrefToRequestData(kind);
+
+    const ajax$ = Observable
+      .ajax(requestData)
+      .map(({ response }) => Object.assign(kind, { response }))
+      .catch(error => this.recoverIfPossible(kind, error));
+
+    return minDur.call(ajax$, this.duration);
+  }
+
+  hrefToRequestData({ href }) {
+    return {
+      method: 'GET',
+      url: href,
+      responseType: 'text',
+    };
+  }
+
+  recoverIfPossible(kind, error) {
+    const { status, xhr } = error;
+
+    if (status && status > 400 && xhr) {
+      // recover with error page returned from server
+      // assuming error page contains the same ids...
+      // TODO: maybe parse and check if error page can be swapped and reload otherwise?
+      return Observable.of(Object.assign(kind, { response: xhr.response }));
+    }
+
+    // TODO: don't throw here?
+    return Observable.throw(error);
+  }
+
+  layPipes() {
+    this.push$$ = new Subject();
+    this.hint$$ = new Subject();
+
+    const push$ = this.push$$.switch();
+    const pop$ = this.bindPopstateEvent();
+
+    this.page$ = Observable.merge(push$, pop$);
+
+    const pauser$ = Observable.defer(() =>
+      Observable.merge(
+        this.page$.map(() => true),
+        this.render$.map(() => false)
+      ).startWith(false)
+    );
+
+    this.hint$ = this.hint$$.switch()
+      .withLatestFrom(pauser$)
+      .switchMap(([hint, paused]) => (paused ?
+        Observable.empty() :
+        Observable.of(hint))
+      );
+
+    // Dream syntax:
+    // this.hint$$.switch().pauseable(pauser$);
+
+    const prefetch$ = Observable.merge(this.hint$, this.page$)
+      .distinctUntilKeyChanged('href')
+      .switchMap(this.fetchPage)
+      .startWith({})
+      .share();
+
+    this.render$ = this.page$
+      .do(this.onBefore)
+      .withLatestFrom(prefetch$)
+      .switchMap(([kind, prefetch]) => (kind.href === prefetch.href ?
+        // Case 1.1: Prefetch already complete, use result
+        Observable.of([kind, prefetch]) :
+        // Case 1.2: Prefetch in progress, use next result
+        prefetch$.take(1).map(fetch => [kind, fetch])
+      ))
+      .do(([kind, prefetch]) => {
+        // try {
+        this.updateDOM(kind, prefetch);
+        this.onAfter();
+        this.renewEventListeners();
+        // } catch (e) {
+        //   this.onError(e);
+        // }
+      })
+      .share();
+
+    this.render$.subscribe(() => {});
+
+    this.renewEventListeners();
+  }
+
+  renewEventListeners() {
     const link$ = this.linkObservable();
-    prefetchHref$$.next(this.bindPrefetchEvents(link$));
+    this.push$$.next(this.bindPushEvents(link$));
+    this.hint$$.next(this.bindHintEvents(link$));
+  }
+
+  updateDOM(kind, { response }) {
+    const { href } = kind;
+    const { title, content } = this.responseToHTML(response);
+
+    if (kind instanceof Push) {
+      window.history.pushState({ id: 'y-smooth-state' }, title, href);
+    }
+
+    this.titleElement.textContent = title;
+    this.resetScrollPostion();
+    this.replaceContent(content);
+  }
+
+  responseToHTML(response) {
+    const documentFragment = this.fragmentFromString(response);
+    const title = this.getTitleFromDocumentFragment(documentFragment);
+    const content = this.getContentFromDocumentFragment(documentFragment);
+    return { title, content };
   }
 
   fragmentFromString(strHTML) {
@@ -185,21 +314,21 @@ export default C => class extends componentCore(C) {
   }
 
   onBefore() {
-    // this.isLoading = true;
+    // this.pauser$.next(true);
     this.el.style.willChange = 'content';
     document.body.style.willChange = 'scroll-position';
     this.fireEvent('before');
   }
 
   onAfter() {
-    // this.isLoading = false;
+    // this.pauser$.next(false);
     this.el.style.willChange = '';
     document.body.style.willChange = '';
     this.fireEvent('after');
   }
 
   onError() {
-    // this.isLoading = false;
+    // this.pauser$.next(false);
     this.el.style.willChange = '';
     document.body.style.willChange = '';
     this.fireEvent('error');
@@ -208,85 +337,21 @@ export default C => class extends componentCore(C) {
   }
 
   // TODO: rename
-  beNice({ event, anchor }) {
+  beNice(event) {
     return (
       !event.metaKey &&
       !event.ctrlKey &&
-      this.beOkay({ anchor })
+      this.beOkay(event)
     );
   }
 
   // TODO: rename
-  beOkay({ anchor }) {
+  beOkay(event) {
+    const anchor = event.currentTarget;
     return (
       anchor != null &&
       shouldLoadAnchor(anchor, this.blacklist, this.hrefRegex)
     );
-  }
-
-  linkObservable() {
-    return Observable.of(this.el.querySelectorAll(this.linkSelector));
-  }
-
-  bindPrefetchEvents(link$) {
-    return Observable.of(
-      link$.map(link => Observable.fromEvent(link, 'mouseenter')),
-      link$.map(link => Observable.fromEvent(link, 'touchstart')),
-      link$.map(link => Observable.fromEvent(link, 'focus'))
-    )
-      .mergeAll()
-      .mergeAll()
-      .map(event => ({ anchor: event.currentTarget }))
-      .filter(this.beOkay)
-      .map(({ anchor }) => anchor.href);
-  }
-
-  hrefToRequestData(hairball) {
-    return Object.assign(hairball, {
-      requestData: {
-        method: 'GET',
-        url: hairball.href,
-        responseType: 'text',
-      },
-    });
-  }
-
-  makeRequest(hairball) {
-    const ajax = Observable
-      .ajax(hairball.requestData)
-      .map(ajaxResponse => Object.assign(hairball, { ajaxResponse }))
-      .catch((error) => {
-        if (error.status && error.status > 400 && error.xhr) {
-          // recover with error page returned from server
-          // assuming error page contains the same ids...
-          // TODO: maybe parse and check if error page can be swapped and reload otherwise?
-          return Observable.of(Object.assign(hairball, { ajaxResponse: error.xhr }));
-        }
-        return Observable.throw(error);
-      });
-      // ::minDur(this.duration)
-    return minDur.call(ajax, this.duration);
-  }
-
-  makePrefetchRequest(hairball) {
-    const { href } = hairball;
-    const ajax = this.makeRequest(hairball).publishLast();
-    const conn = ajax.connect();
-    return { href, ajax, conn };
-  }
-
-  usePrefechOrMakeRequest([hairball, prefetch]) {
-    // prefetch request matches user intent
-    if (hairball.href && prefetch.href && hairball.href === prefetch.href) {
-      return prefetch.ajax
-        // copy properties of original req
-        .map(h => Object.assign(h, hairball));
-    }
-    // current request doesn't match prefetch
-    // unsubscribe/cancel prefetch
-    if (prefetch.conn) prefetch.conn.unsubscribe();
-    // make new, correct request
-    return this.makeRequest(hairball);
   }
 
   getTitleFromDocumentFragment(documentFragment) {
@@ -299,28 +364,6 @@ export default C => class extends componentCore(C) {
     }
 
     return documentFragment.getElementById(this.el.id);
-  }
-
-  ajaxResponseToContent(hairball) {
-    const { href, ajaxResponse: { response } } = hairball;
-
-    const documentFragment = this.fragmentFromString(response);
-    const title = this.getTitleFromDocumentFragment(documentFragment);
-    const content = this.getContentFromDocumentFragment(documentFragment);
-
-    return Object.assign(hairball, { title, href, content });
-  }
-
-  updateDOM({ title, content, href, push }) {
-    // update title separately
-    // TODO: update meta description?
-    this.titleElement.textContent = title;
-
-    // push new frame to history if not a popstate
-    if (push) window.history.pushState({}, title, href);
-
-    this.resetScrollPostion();
-    this.replaceContent(content);
   }
 
   replaceContent(content) {

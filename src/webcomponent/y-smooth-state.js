fe5c6f83d78e47bca3992bb923dc9b8aa4d0a984
Florian Klampfer <f.klampfer@gmail.com>
2016-10-11 11:20:39 +0200

    Make importable in old browsers

--- src/webcomponent/y-smooth-state.js
+++ src/webcomponent/y-smooth-state.js
@@ -8,9 +8,14 @@ import htmlElement from 'y-component/src/html-element';
 
 import smoothStateCore from '../core';
 
+// Older browsers don't treat `HTMLElement` as a class,
+// which would cause an exception from just importing the class below (even when never using it)
+// Replacing it with a dummy function prevents this.
+const HTMLElement = typeof global.HTMLElement === 'function' ? global.HTMLElement : () => {};
+
 export default class extends htmlElement(smoothStateCore(HTMLElement)) {
   // @override
-  setupDOM(el) { // don't use shadow DOM
+  setupDOM(el) { // prevents shadow DOM creation
     return el;
   }
 }

4f3d47ac00871a7ce88a4c2d128a412d8423d4fa
Florian Klampfer <mail@qwtel.com>
2018-02-14 13:31:37 +0100

    import/export qd-set

--- src/common.js
+++ src/common.js
@@ -14,6 +14,10 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import { Set } from 'qd-set';
+
+export { Set };
+
 export const matches =
   Element.prototype.matches ||
   Element.prototype.matchesSelector ||

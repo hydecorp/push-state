933c0a1adf82d7105142993af2d37259a7df81e6
Florian Klampfer <mail@qwtel.com>
2018-03-24 13:08:22 +0000

    fix import URL

--- src/mixin/methods.js
+++ src/mixin/methods.js
@@ -15,6 +15,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import { isExternal, isHash } from "../common";
+import { URL } from "../url";
 
 import { PUSH, POP } from "./constants";
 

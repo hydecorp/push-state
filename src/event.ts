ae3f37af2643df73106d663afa90b7831fe84cd8
Florian Klampfer <mail@qwtel.com>
2019-09-01 14:50:08 +0700

    Add back error msgs

--- src/event.ts
+++ src/event.ts
@@ -28,45 +28,37 @@ export class EventManager {
   }
 
   emitDOMError(context) {
-    console.error(context);
     const { replaceElMissing, url } = context;
 
     if (replaceElMissing) {
-      // if (process.env.DEBUG) {
-      //   const ids = this.replaceIds
-      //     .concat(this.el.id || [])
-      //     .map(x => `#${x}`)
-      //     .join(", ");
-      //   console.warn(
-      //     `Couldn't find one or more ids of '${ids}' in the document at '${
-      //     window.location
-      //     }'. Opening the link directly.`
-      //   );
-      // }
+      if (process.env.DEBUG) {
+        console.warn(
+          `Couldn't find one or more element in the document at '${location}'. Opening the link directly.`
+        );
+      }
 
       // To open the link directly, we first pop one entry off the browser history.
       // We have to do this because (some) browsers won't handle the back button correctly otherwise.
       // We then wait for a short time and change the document's location.
       // TODO: If we didn't call `pushState` optimistically we wouldn't have to do this.
       window.history.back();
-      setTimeout(() => (document.location.href = url), 100);
+      setTimeout(() => document.location.assign(url), 100);
 
       // If it's a different error, throw the generic `error` event.
     } else {
-      // if (process.env.DEBUG) console.error(context);
       this.parent.dispatchEvent(new CustomEvent('error', { detail: context }));
+      if (process.env.DEBUG) console.error(context);
     }
   }
 
   emitNetworkError(context) {
-    // if (process.env.DEBUG) console.error(context);
     this.parent.dispatchEvent(new CustomEvent('networkerror', { detail: context }));
+    if (process.env.DEBUG) console.error(context);
   }
 
   emitError(context) {
-    console.log(context.error);
-    // if (process.env.DEBUG) console.error(context);
     this.parent.dispatchEvent(new CustomEvent('error', { detail: context }));
+    if (process.env.DEBUG) console.error(context);
   }
 
   emitReady(context) {

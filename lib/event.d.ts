64722e52db16765fcc8136ae5f0cb82678b662ac
Florian Klampfer <mail@qwtel.com>
2020-06-21 18:56:30 +0700

    1.0.0-alpha.4

---
+++ lib/event.d.ts
@@ -0,0 +1,14 @@
+import { Context } from './common';
+import { HyPushState } from "./index";
+export declare class EventManager {
+    private parent;
+    constructor(parent: HyPushState);
+    onStart(context: Context): void;
+    emitDOMError(context: any): void;
+    emitNetworkError(context: any): void;
+    emitError(context: any): void;
+    emitReady(context: any): void;
+    emitAfter(context: any): void;
+    emitProgress(context: any): void;
+    emitLoad(context: any): void;
+}
---
+++ lib/rewrite-urls.d.ts
@@ -0,0 +1,1 @@
+export declare function rewriteURLs(replaceEls: Element[], base: string): void;

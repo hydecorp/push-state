9496aabfb7e77e51d0ea571fa6a02d91b620c3ec
Florian Klampfer <mail@qwtel.com>
2020-06-29 14:57:19 +0700

    1.0.0-alpha.7

---
+++ lib/fetch.d.ts
@@ -0,0 +1,15 @@
+import { Observable } from "rxjs";
+import { Context } from "./common";
+import { HyPushState } from './index';
+export interface ResponseContext extends Context {
+    responseText: string | null;
+    error?: any;
+}
+export declare class FetchManager {
+    private parent;
+    constructor(parent: HyPushState);
+    fetchPage(context: Context): Observable<ResponseContext>;
+    private selectPrefetch;
+    getResponse(prefetch$: Observable<ResponseContext>, context: Context, latestPrefetch: ResponseContext): Observable<ResponseContext>;
+}
+//# sourceMappingURL=fetch.d.ts.map
---
+++ lib/history.d.ts
@@ -0,0 +1,14 @@
+import { Context } from "./common";
+import { ReplaceContext } from "./update";
+export declare class HistoryManager {
+    private parent;
+    constructor(parent: Location & {
+        histId: string;
+        simulateHashChange: boolean;
+    });
+    updateHistoryState({ cause, replace, url, oldURL }: Context): void;
+    updateTitle({ cause, title }: ReplaceContext): void;
+    updateHistoryScrollPosition: () => void;
+    private assignScrollPosition;
+}
+//# sourceMappingURL=history.d.ts.map
